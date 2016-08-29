93b8b10cb81f6bb510581a351eeb4d4d04216594
Nat Guy <nguy@jpl.nasa.gov>
2016-08-29 10:25:30 -0700

    Added some simple LOD on ISS model.

---
+++ Earth Orbit Demo/Assets/ISSLOD.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ISSLOD : MonoBehaviour
+{
+
+    public float levelOfDetailTransitionDistance;
+    public GameObject highLODModel;
+    public GameObject lowLODModel;
+
+    private bool usingHighLOD;
+
+	// Use this for initialization
+	void Start()
+    {
+        usingHighLOD = false;
+
+        highLODModel.SetActive(usingHighLOD);
+        lowLODModel.SetActive(!usingHighLOD);
+    }
+	
+	// Update is called once per frame
+	void Update()
+    {
+        float cameraDistanceToISS = Vector3.Distance(Camera.main.transform.position, this.transform.position);
+
+        usingHighLOD = (cameraDistanceToISS < levelOfDetailTransitionDistance);
+
+        highLODModel.SetActive(usingHighLOD);
+        lowLODModel.SetActive(!usingHighLOD);
+    }
+
+    
+}
